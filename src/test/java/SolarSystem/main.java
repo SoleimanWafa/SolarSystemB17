8d612188ac41bc45ee43f6a6b99994a3db497afe
Soleiman <solwafa@gmail.com>
2023-11-03 22:39:10 -0700

    i have added planet 2

--- src/test/java/SolarSystem/main.java
+++ src/test/java/SolarSystem/main.java
@@ -14,5 +14,13 @@ public class main {
         Sun.name="panther";
         Sun.color="black";
         Sun.temp="1223124kelvin";
+
+        //  ***********Planet2************
+
+        Planet2 xaros = new Planet2();
+        xaros.name="nehaio";
+        xaros.color="blue";
+        xaros.size="3242343244324km";
+
     }
 }
